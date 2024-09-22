d07365ed4a1e985528ea583effd036a8062b34c7
Kalmit Kulkarni <89975943+TechKnight10@users.noreply.github.com>
2024-09-22 13:58:41 +0530

    Add files via upload

---
+++ randomsampling telugu/telugu.py
@@ -0,0 +1,25 @@
+import random
+from datasets import load_dataset
+import openpyxl
+
+dataset = load_dataset("csebuetnlp/xlsum", "telugu", split="test")
+
+random_indices = random.sample(range(len(dataset)), 150)
+
+sampled_data = dataset.select(random_indices)
+
+for i, article in enumerate(sampled_data):
+    print(f"Article {i+1} ID: {article['id']}, URL: {article['url']}")
+
+
+output_file = "urls_and_ids telugu.txt"
+with open(output_file, "w") as file:
+    for article in sampled_data:
+        file.write(f"ID: {article['id']}, URL: {article['url']}\n")
+excel_file = "urls_and_ids telugu.xlsx"
+wb = openpyxl.Workbook()
+ws = wb.active
+ws['A1'] = f'=HYPERLINK("{output_file}", "Click here for URLs and IDs for telugu")'
+
+wb.save(excel_file)
+print(f"Excel file saved: {excel_file}")
